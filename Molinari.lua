59b819fbfc38e85575a4e214674c2b73c2d81f27
Adrian L Lange <adrianlund@gmail.com>
2010-03-01 18:09:51 +0100

    Removed disenchanting support - See project "Dolemite"

--- Molinari.lua
+++ Molinari.lua
@@ -1,57 +1,10 @@
-﻿local ARMORTYPE
-if(GetLocale() == 'ruRU') then
-	ARMORTYPE = 'Доспехи' -- DIAF Blizzard
-else
-	ARMORTYPE = ARMOR
-end
+﻿local addon = ...
 
-local button = CreateFrame('Button', 'Molinari', UIParent, 'SecureActionButtonTemplate, AutoCastShineTemplate')
-button:RegisterForClicks('LeftButtonUp')
-button:SetAttribute('*type*', 'macro')
-button:Hide()
-
-for _, spark in pairs(button.sparkles) do
-	spark:SetHeight(spark:GetHeight() * 3)
-	spark:SetWidth(spark:GetWidth() * 3)
-end
-
-button:SetFrameStrata('DIALOG')
-button:RegisterEvent('PLAYER_LOGIN')
-button:RegisterEvent('MODIFIER_STATE_CHANGED')
-button:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
-button:SetScript('OnLeave', function(self)
-	if(InCombatLockdown()) then
-		self:RegisterEvent('PLAYER_REGEN_ENABLED')
-	else
-		self:Hide()
-		self:ClearAllPoints()
-		AutoCastShine_AutoCastStop(self)
-	end
-end)
-
-local disenchanting = GetSpellInfo(13262)
+local button = CreateFrame('Button', addon, UIParent, 'SecureActionButtonTemplate, AutoCastShineTemplate')
 local macro = '/cast %s\n/use %s %s'
 local spells = {}
 
-function button:MODIFIER_STATE_CHANGED(event, key)
-	if(self:IsShown() and (key == 'LALT' or key == 'RALT')) then
-		self:GetScript('OnLeave')(self)
-	end
-end
-
-function button:PLAYER_REGEN_ENABLED(event)
-	self:UnregisterEvent(event)
-	self:GetScript('OnLeave')(self)
-end
-
-local function Disenchantable(item)
-	local _, _, quality, _, _, type = GetItemInfo(item)
-	if((type == ARMORTYPE or type == ENCHSLOT_WEAPON) and quality > 1 and quality < 5) then
-		return disenchanting, 0.5, 0.5, 1
-	end
-end
-
-local function ScanTooltip()
+local function ScanTooltip(text)
 	for index = 1, GameTooltip:NumLines() do
 		local info = spells[_G['GameTooltipTextLeft'..index]:GetText()]
 		if(info) then
@@ -64,16 +17,33 @@ local function Clickable()
 	return not InCombatLockdown() and IsAltKeyDown()
 end
 
+local function Disperse(self)
+	if(InCombatLockdown()) then
+		self:RegisterEvent('PLAYER_REGEN_ENABLED')
+	else
+		self:Hide()
+		self:ClearAllPoints()
+		AutoCastShine_AutoCastStop(self)
+	end
+end
+
+function button:MODIFIER_STATE_CHANGED(event, key)
+	if(self:IsShown() and (key == 'LALT' or key == 'RALT')) then
+		Disperse(self)
+	end
+end
+
+function button:PLAYER_REGEN_ENABLED(event)
+	self:UnregisterEvent(event)
+	Disperse(self)
+end
+
 GameTooltip:HookScript('OnTooltipSetItem', function(self)
 	local item = self:GetItem()
 	if(item and Clickable()) then
 		local spell, r, g, b = ScanTooltip()
-		if(not spell) then
-			spell, r, g, b = Disenchantable(item)
-		end
-
-		local bag, slot = GetMouseFocus():GetParent(), GetMouseFocus()
-		if(spell and GetContainerItemInfo(bag:GetID(), slot:GetID()) and bag ~= PaperDollFrameItemFlyoutButtons) then
+		if(spell) then
+			local bag, slot = GetMouseFocus():GetParent(), GetMouseFocus()
 			button:SetAttribute('macrotext', macro:format(spell, bag:GetID(), slot:GetID()))
 			button:SetAllPoints(slot)
 			button:Show()
@@ -90,8 +60,20 @@ function button:PLAYER_LOGIN()
 	if(IsSpellKnown(31252)) then
 		spells[ITEM_PROSPECTABLE] = {GetSpellInfo(31252), 1, 0.5, 0.5}
 	end
+end
 
-	if(not IsSpellKnown(13262)) then
-		Disenchantable = function() end
+do
+	button:SetScript('OnLeave', Disperse)
+	button:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
+	button:SetFrameStrata('DIALOG')
+	button:RegisterEvent('MODIFIER_STATE_CHANGED')
+	button:RegisterEvent('PLAYER_LOGIN')
+	button:RegisterForClicks('LeftButtonUp')
+	button:SetAttribute('*type*', 'macro')
+	button:Hide()
+
+	for _, sparks in pairs(button.sparkles) do
+		sparks:SetHeight(sparks:GetHeight() * 3)
+		sparks:SetWidth(sparks:GetWidth() * 3)
 	end
 end
