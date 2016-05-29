8c0a04fa7110c3691e14e4e80f1fd37c2f811b53
Adrian L Lange <mail@p3lim.net>
2016-05-29 22:34:51 +0200

    Enable localization
    This is handled by the CurseForge packager and localization system

--- Localization.lua
+++ Localization.lua
@@ -5,3 +5,26 @@ setmetatable(L, {__index = function(L, key)
 	L[key] = value
 	return value
 end})
+
+local locale = GetLocale()
+if(locale == 'deDE') then
+	--@localization(locale="deDE", format="lua_additive_table")@
+elseif(locale == 'esES') then
+	--@localization(locale="esES", format="lua_additive_table")@
+elseif(locale == 'esMX') then
+	--@localization(locale="esMX", format="lua_additive_table")@
+elseif(locale == 'frFR') then
+	--@localization(locale="frFR", format="lua_additive_table")@
+elseif(locale == 'itIT') then
+	--@localization(locale="itIT", format="lua_additive_table")@
+elseif(locale == 'koKR') then
+	--@localization(locale="koKR", format="lua_additive_table")@
+elseif(locale == 'ptBR') then
+	--@localization(locale="ptBR", format="lua_additive_table")@
+elseif(locale == 'ruRU') then
+	--@localization(locale="ruRU", format="lua_additive_table")@
+elseif(locale == 'zhCN') then
+	--@localization(locale="zhCN", format="lua_additive_table")@
+elseif(locale == 'zhTW') then
+	--@localization(locale="zhTW", format="lua_additive_table")@
+end
