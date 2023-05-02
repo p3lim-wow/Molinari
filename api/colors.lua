a020bb75f80db46ba08c0e4a90de91b917d44740
Adrian L Lange <git@p3lim.net>
2023-05-02 18:00:03 +0200

    Add table for colors

---
+++ api/colors.lua
@@ -0,0 +1,9 @@
+local _, addon = ...
+
+addon.colors = {}
+addon.colors.prospectable = CreateColor(1, 1/3, 1/3)
+addon.colors.millable = CreateColor(1/2, 1, 1/2)
+addon.colors.openable = CreateColor(0, 1, 1)
+addon.colors.scrappable = CreateColor(1/5, 1/5, 1/5)
+addon.colors.crushable = CreateColor(1, 1/3, 1/3)
+addon.colors.disenchantable = CreateColor(1/2, 1/2, 1)
