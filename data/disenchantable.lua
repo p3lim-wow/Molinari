a5a8ee75917bc73052c55eb1a60e8df7aa1ce2ad
p3lim <p3lim@users.noreply.github.com>
2023-05-02 15:35:02 +0000

    Update retail data to 10.0.7.49343

---
+++ data/disenchantable.lua
@@ -0,0 +1,31 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.disenchantable = {
+	[137195] = true, -- Highmountain Armor
+	[137221] = true, -- Enchanted Raven Sigil
+	[137286] = true, -- Fel-Crusted Rune
+	[181991] = true, -- Antique Stalker's Bow
+	[182021] = true, -- Antique Kyrian Javelin
+	[182043] = true, -- Antique Necromancer's Staff
+	[182067] = true, -- Antique Duelist's Rapier
+	[198675] = true, -- Lava-Infused Seed
+	[198689] = true, -- Stormbound Horn
+	[198694] = true, -- Enriched Earthen Shard
+	[198798] = true, -- Flashfrozen Scroll
+	[198799] = true, -- Forgotten Arcane Tome
+	[198800] = true, -- Fractured Titanic Sphere
+	[200479] = true, -- Sophic Amalgamation
+	[200939] = true, -- Chromatic Pocketwatch
+	[200940] = true, -- Everflowing Inkwell
+	[200941] = true, -- Seal of Order
+	[200942] = true, -- Vibrant Emulsion
+	[200943] = true, -- Whispering Band
+	[200945] = true, -- Valiant Hammer
+	[200946] = true, -- Thunderous Blade
+	[200947] = true, -- Carving of Awakening
+	[201356] = true, -- Glimmer of Fire
+	[201357] = true, -- Glimmer of Frost
+	[201358] = true, -- Glimmer of Air
+	[201359] = true, -- Glimmer of Earth
+	[201360] = true, -- Glimmer of Order
+}
