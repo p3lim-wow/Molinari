cd356cb8c7c45e0ff98a68919c6d2d1b29ccab52
p3lim <p3lim@users.noreply.github.com>
2023-05-02 15:35:02 +0000

    Update retail data to 10.0.7.49343

---
+++ data/keys.lua
@@ -0,0 +1,25 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.keys = {
+	[4367] = {15, 2506, 100, 0}, -- Small Seaforium Charge
+	[4398] = {20, 2506, 200, 0}, -- Large Seaforium Charge
+	[15869] = {15, 2477, 100, 0}, -- Silver Skeleton Key
+	[15870] = {15, 2477, 150, 0}, -- Golden Skeleton Key
+	[15871] = {20, 2477, 200, 0}, -- Truesilver Skeleton Key
+	[15872] = {30, 2477, 275, 0}, -- Arcanite Skeleton Key
+	[18594] = {30, 2506, 275, 0}, -- Powerful Seaforium Charge
+	[23819] = {30, 2505, 50, 0}, -- Elemental Seaforium Charge
+	[43853] = {30, 2475, 55, 0}, -- Titanium Skeleton Key
+	[43854] = {30, 2475, 1, 0}, -- Cobalt Skeleton Key
+	[55053] = {35, 2474, 25, 0}, -- Obsidium Skeleton Key
+	[60853] = {35, 2503, 1, 0}, -- Volatile Seaforium Blastpack
+	[77532] = {35, 2502, 1, 0}, -- Locksmith's Powderkeg
+	[82960] = {35, 2473, 1, 0}, -- Ghostly Skeleton Key
+	[109645] = {500, 2501, 1, 0}, -- Blackrock Seaforium
+	[130250] = {550, 2518, 1, 0}, -- Jeweled Lockpick
+	[159825] = {50, 2507, 1, 0}, -- Scroll of Unlocking
+	[159826] = {50, 2437, 1, 0}, -- Monelite Skeleton Key
+	[171441] = {60, 2751, 1, 51}, -- Laestrite Skeleton Key
+	[173065] = {60, 2756, 1, 51}, -- Writ of Grave Robbing
+	[191256] = {70, 2822, 1, 61}, -- Serevite Skeleton Key
+}
---
+++ data/scrappable.lua
@@ -0,0 +1,5 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.scrappable = {
+	[198651] = 382374, -- Piece of Scrap
+}
