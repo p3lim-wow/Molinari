c63cfa5cc7e55fd04fe32fff86ad24915581e1cc
p3lim <p3lim@users.noreply.github.com>
2023-05-02 16:36:36 +0000

    Update vanilla data to 1.14.3.49229

---
+++ data/keys_vanilla.lua
@@ -0,0 +1,8 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.keys = {
+	[15869] = {25, 164, 100, 0}, -- Silver Skeleton Key
+	[15870] = {125, 164, 150, 0}, -- Golden Skeleton Key
+	[15871] = {200, 164, 200, 0}, -- Truesilver Skeleton Key
+	[15872] = {300, 164, 275, 0}, -- Arcanite Skeleton Key
+}
