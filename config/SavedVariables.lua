4a9ac3c1fec8dd4fdb4b764f64fa1a22680410e9
Adrian L Lange <git@p3lim.net>
2023-05-03 15:16:01 +0200

    Fix settings not loading

--- config/SavedVariables.lua
+++ config/SavedVariables.lua
@@ -14,7 +14,11 @@ local defaults = {
 	},
 }
 
-addon:HookAddOn(addonName, function()
-	-- initialize database with defaults
-	addon.db = LibStub('AceDB-3.0'):New('MolinariDB2', defaults, true)
-end)
+function addon:ADDON_LOADED(name)
+	if name == addonName then
+		-- initialize database with defaults
+		addon.db = LibStub('AceDB-3.0'):New('MolinariDB2', defaults, true)
+
+		return true
+	end
+end
