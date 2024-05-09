880d729e0535161b7e0401d636aa895a9b795f4d
Adrian L Lange <git@p3lim.net>
2024-05-09 16:21:00 +0200

    Revert "Update wrath data to 4.4.0.54377"
    This reverts commit c5d24762dab024c54526422db893468581b2aa50.
    
    # Conflicts:
    #	data/keys_wrath.lua
    #	data/nondisenchantable_wrath.lua
    #	data/openable_wrath.lua
    #	data/prospectable_wrath.lua

---
+++ data/prospectable_classic.lua
@@ -0,0 +1,14 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.prospectable = {
+	[2770] = 1, -- Copper Ore
+	[2771] = 50, -- Tin Ore
+	[2772] = 125, -- Iron Ore
+	[3858] = 175, -- Mithril Ore
+	[10620] = 250, -- Thorium Ore
+	[23424] = 275, -- Fel Iron Ore
+	[23425] = 325, -- Adamantite Ore
+	[36909] = 350, -- Cobalt Ore
+	[36910] = 450, -- Titanium Ore
+	[36912] = 400, -- Saronite Ore
+}
