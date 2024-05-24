134a7b34428c252fde99cf7a923f88077747490f
Adrian L Lange <git@p3lim.net>
2024-05-25 00:39:51 +0200

    Chinese translations by @seakoria
    Fixes #148
    Fixes #149

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -6,8 +6,8 @@ L['ALT + SHIFT key'] = _G.ALT_KEY_TEXT .. ' + ' .. _G.SHIFT_KEY
 L['You can\'t do that while in combat'] = _G.ERR_NOT_IN_COMBAT
 
 -- config
--- L['Modifier to activate %s'] -- MISSING! %s = "Molinari"
--- L['Item Blocklist'] = '' -- MISSING!
--- L['Block Item'] = '' -- MISSING!
--- L['Items in this list will not be processed.'] = '' -- MISSING!
--- L['Block a new item by ID'] = '' -- MISSING!
+L['Modifier to activate %s'] = '启用%s的设置选项'
+L['Item Blocklist'] = '物品屏蔽列表'
+L['Block Item'] = '屏蔽物品'
+L['Items in this list will not be processed.'] = '列表中的物品不会被处理'
+L['Block a new item by ID'] = '通过物品ID屏蔽一个新的物品'
--- locale/zhTW.lua
+++ locale/zhTW.lua
@@ -6,8 +6,8 @@ L['ALT + SHIFT key'] = _G.ALT_KEY_TEXT .. ' + ' .. _G.SHIFT_KEY
 L['You can\'t do that while in combat'] = _G.ERR_NOT_IN_COMBAT
 
 -- config
--- L['Modifier to activate %s'] -- MISSING! %s = "Molinari"
--- L['Item Blocklist'] = '' -- MISSING!
--- L['Block Item'] = '' -- MISSING!
--- L['Items in this list will not be processed.'] = '' -- MISSING!
--- L['Block a new item by ID'] = '' -- MISSING!
+L['Modifier to activate %s'] = '啟用%s的設置選項'
+L['Item Blocklist'] = '物品屏蔽列表'
+L['Block Item'] = '屏蔽物品'
+L['Items in this list will not be processed.'] = '列表中的物品不會被處理'
+L['Block a new item by ID'] = '通過物品ID屏蔽一個新的物品'
