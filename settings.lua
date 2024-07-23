6b48532a8f31c592d84648a7ff3ea062c341b729
Adrian L Lange <git@p3lim.net>
2024-07-23 22:16:37 +0200

    Migrate settings from old Ace config

--- settings.lua
+++ settings.lua
@@ -18,5 +18,15 @@ addon:RegisterSettings('MolinariDB3', {
 addon:RegisterSettingsSlash('/molinari')
 
 function addon:OnLoad()
-	-- TODO: migrate old settings
+	-- migrate old settings
+	-- TODO: remove this in 12.x
+	if MolinariDB2 and MolinariDB2.profiles and MolinariDB2.profiles.Default then
+		-- I never implemented profiles so everything will be in the default profile
+		if MolinariDB2.profiles.Default.modifier ~= nil then
+			addon:SetOption('modifier', MolinariDB2.profiles.Default.modifier)
+			addon:Print("migrated setting 'modifier' from old savedvariables")
+		end
+	end
+	-- TODO: this needs testing, it assumes MolinariDB3 is loaded with defaults at this point
+	MolinariDB2 = nil
 end
