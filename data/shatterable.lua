35b35ef6ea1365cea258df89327b0edf0cdaf9f3
p3lim <26496+p3lim@users.noreply.github.com>
2024-09-09 21:52:22 +0000

    Update retail data to 11.0.2.56313

---
+++ data/shatterable.lua
@@ -0,0 +1,17 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.shatterable = {
+	[124441] = {224199, 1}, -- Leylight Shard
+	[124442] = {252106, 1}, -- Chaos Crystal
+	[190316] = {391304, 1}, -- Awakened Earth
+	[190321] = {391304, 1}, -- Awakened Fire
+	[190324] = {391304, 1}, -- Awakened Order
+	[190327] = {391304, 1}, -- Awakened Air
+	[190329] = {391304, 1}, -- Awakened Frost
+	[194124] = {391302, 1}, -- Vibrant Shard
+	[200113] = {391302, 1}, -- Resonant Crystal
+	[213610] = {445466, 1}, -- Crystalline Powder
+	[213611] = {445466, 1}, -- Writhing Sample
+	[213612] = {445466, 1}, -- Viridescent Spores
+	[213613] = {445466, 1}, -- Leyline Residue
+}
