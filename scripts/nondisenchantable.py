308914d42eedce72ab2c81cf204c0e614d74934c
Adrian L Lange <git@p3lim.net>
2024-09-10 19:11:21 +0200

    Filter out non-equippable items from nondisenchantable database
    In most, if not all cases non-equippable items won't be disenchantable
    anyways, and it's something we kind-of target anyways in the API.

--- scripts/nondisenchantable.py
+++ scripts/nondisenchantable.py
@@ -7,15 +7,15 @@ itemSparse = CSVReader(open('dbc/itemsparse.csv', 'r'))
 items = {}
 # iterate through ItemSparse for items that can't be disenchanted
 for row in itemSparse:
-	if (getattr(row, 'Flags[0]') & 0x10) != 0:
-		# deprecated item
+	if (getattr(row, 'Flags[0]') & 0x10) != 0: # deprecated item
 		continue
 
-	if (getattr(row, 'Flags[0]') & 0x8000) != 0:
-		items[row.ID] = {
-			'itemID': row.ID,
-			'name': row.Display_lang.strip(),
-		}
+	if (getattr(row, 'Flags[0]') & 0x8000) != 0: # "No Disenchant" flag
+		if row.InventoryType != 0: # equippable
+			items[row.ID] = {
+				'itemID': row.ID,
+				'name': row.Display_lang.strip(),
+			}
 
 # print data file structure
 templateLuaTable('nondisenchantable', '\t[{itemID}] = true, -- {name}', items)
