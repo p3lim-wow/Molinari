d6d280166da02ab5ba3471c40b32c7d05f3dcd92
Adrian L Lange <git@p3lim.net>
2024-09-10 19:16:07 +0200

    Check for quality too

--- scripts/nondisenchantable.py
+++ scripts/nondisenchantable.py
@@ -7,15 +7,26 @@ itemSparse = CSVReader(open('dbc/itemsparse.csv', 'r'))
 items = {}
 # iterate through ItemSparse for items that can't be disenchanted
 for row in itemSparse:
-	if (getattr(row, 'Flags[0]') & 0x10) != 0: # deprecated item
+	if (getattr(row, 'Flags[0]') & 0x10) != 0:
+		# deprecated item
 		continue
 
-	if (getattr(row, 'Flags[0]') & 0x8000) != 0: # "No Disenchant" flag
-		if row.InventoryType != 0 and row.InventoryType != 4: # equippable and not a shirt
-			items[row.ID] = {
-				'itemID': row.ID,
-				'name': row.Display_lang.strip(),
-			}
+	if (getattr(row, 'Flags[0]') & 0x8000) == 0:
+		# "No Disenchant" flag
+		continue
+
+	if row.OverallQualityID < 2 or row.OverallQualityID > 4:
+		# can't disenchant items of too low or too high quality anyways
+		continue
+
+	if row.InventoryType == 0 or row.InventoryType == 4:
+		 # not equippable or a shirt
+		 continue
+
+	items[row.ID] = {
+		'itemID': row.ID,
+		'name': row.Display_lang.strip(),
+	}
 
 # print data file structure
 templateLuaTable('nondisenchantable', '\t[{itemID}] = true, -- {name}', items)
