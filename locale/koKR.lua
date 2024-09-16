0cb41254e6d47c7617e2955e401e6c3438369e40
Adrian L Lange <git@p3lim.net>
2024-09-16 23:36:37 +0200

    Add koKR translation
    Fixes #180

--- locale/koKR.lua
+++ locale/koKR.lua
@@ -4,4 +4,4 @@ local L = select(2, ...).L('koKR')
 L['ALT key'] = ALT_KEY
 L['ALT + CTRL key'] = ALT_KEY_TEXT .. ' + ' .. CTRL_KEY
 L['ALT + SHIFT key'] = ALT_KEY_TEXT .. ' + ' .. SHIFT_KEY
--- L['Modifier to activate %s'] -- MISSING! %s = "Molinari"
+L['Modifier to activate %s'] = '몰리나리를 작동시키는 수식어'
