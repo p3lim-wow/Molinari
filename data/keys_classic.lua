ef4d8c4808fd7649089c5e91c5bc37ba33dd6263
p3lim <26496+p3lim@users.noreply.github.com>
2025-07-10 08:19:11 +0000

    Update classic data to 3.4.5.61937

--- data/keys_classic.lua
+++ data/keys_classic.lua
@@ -1,18 +1,14 @@
 -- this file is auto-generated
 local _, addon = ...
 addon.data.keys = {
-	[4367] = {150, 202, 100, 0}, -- Small Seaforium Charge
-	[4398] = {250, 202, 200, 0}, -- Large Seaforium Charge
-	[15869] = {25, 164, 100, 0}, -- Silver Skeleton Key
-	[15870] = {125, 164, 150, 0}, -- Golden Skeleton Key
-	[15871] = {200, 164, 200, 0}, -- Truesilver Skeleton Key
-	[15872] = {300, 164, 275, 0}, -- Arcanite Skeleton Key
-	[18594] = {300, 202, 275, 0}, -- Powerful Seaforium Charge
-	[23819] = {350, 202, 350, 0}, -- Elemental Seaforium Charge
-	[43853] = {400, 164, 430, 0}, -- Titanium Skeleton Key
-	[43854] = {375, 164, 350, 0}, -- Cobalt Skeleton Key
-	[55053] = {425, 164, 475, 0}, -- Obsidium Skeleton Key
-	[60853] = {400, 202, 425, 0}, -- Volatile Seaforium Blastpack
-	[77532] = {450, 202, 525, 0}, -- Locksmith's Powderkeg
-	[82960] = {450, 164, 500, 0}, -- Ghostly Skeleton Key
+	[4367] = {149, 202, 100, 0}, -- Small Seaforium Charge
+	[4398] = {249, 202, 200, 0}, -- Large Seaforium Charge
+	[15869] = {24, 164, 100, 0}, -- Silver Skeleton Key
+	[15870] = {124, 164, 150, 0}, -- Golden Skeleton Key
+	[15871] = {199, 164, 200, 0}, -- Truesilver Skeleton Key
+	[15872] = {299, 164, 275, 0}, -- Arcanite Skeleton Key
+	[18594] = {299, 202, 275, 0}, -- Powerful Seaforium Charge
+	[23819] = {349, 202, 350, 0}, -- Elemental Seaforium Charge
+	[43853] = {399, 164, 430, 0}, -- Titanium Skeleton Key
+	[43854] = {374, 164, 350, 0}, -- Cobalt Skeleton Key
 }
--- data/prospectable_classic.lua
+++ data/prospectable_classic.lua
@@ -12,12 +12,4 @@ addon.data.prospectable = {
 	[36910] = 450, -- Titanium Ore
 	[36911] = 400, -- Azurite Ore
 	[36912] = 400, -- Saronite Ore
-	[52183] = 500, -- Pyrite Ore
-	[52184] = 425, -- Black Iron Ore
-	[52185] = 475, -- Elementium Ore
-	[53038] = 425, -- Obsidium Ore
-	[72092] = 500, -- Ghost Iron Ore
-	[72093] = 550, -- Kyparite
-	[72094] = 600, -- Black Trillium Ore
-	[72103] = 600, -- White Trillium Ore
 }
