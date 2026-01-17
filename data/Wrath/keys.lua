124eaa81e3d568dde920b581ebe2db40e413bae5
p3lim <26496+p3lim@users.noreply.github.com>
2026-01-17 13:17:48 +0000

    Update wrath data to 3.80.0.65301

---
+++ data/Wrath/keys.lua
@@ -0,0 +1,14 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.keys = {
+	[4367] = {149, 202, 100, 0}, -- Small Seaforium Charge
+	[4398] = {249, 202, 200, 0}, -- Large Seaforium Charge
+	[15869] = {24, 164, 100, 0}, -- Silver Skeleton Key
+	[15870] = {124, 164, 150, 0}, -- Golden Skeleton Key
+	[15871] = {199, 164, 200, 0}, -- Truesilver Skeleton Key
+	[15872] = {299, 164, 275, 0}, -- Arcanite Skeleton Key
+	[18594] = {299, 202, 275, 0}, -- Powerful Seaforium Charge
+	[23819] = {349, 202, 350, 0}, -- Elemental Seaforium Charge
+	[43853] = {399, 164, 430, 0}, -- Titanium Skeleton Key
+	[43854] = {374, 164, 350, 0}, -- Cobalt Skeleton Key
+}
