ea2fbe56f9db789014bd6dc2913a5ee94f1511a1
p3lim <26496+p3lim@users.noreply.github.com>
2026-01-17 13:17:45 +0000

    Update tbc data to 2.5.5.65417

---
+++ data/TBC/keys.lua
@@ -0,0 +1,12 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.keys = {
+	[4367] = {149, 202, 100, 0}, -- Small Seaforium Charge
+	[4398] = {249, 202, 200, 0}, -- Large Seaforium Charge
+	[15869] = {24, 164, 100, 0}, -- Silver Skeleton Key
+	[15870] = {124, 164, 150, 0}, -- Golden Skeleton Key
+	[15871] = {199, 164, 200, 0}, -- Truesilver Skeleton Key
+	[15872] = {299, 164, 275, 0}, -- Arcanite Skeleton Key
+	[18594] = {299, 202, 275, 0}, -- Powerful Seaforium Charge
+	[23819] = {349, 202, 350, 0}, -- Elemental Seaforium Charge
+}
