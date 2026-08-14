0119486f4cf4fb845f024b488c9a635dca3a80ff
p3lim <26496+p3lim@users.noreply.github.com>
2026-08-14 09:01:28 +0000

    Update retail data to 12.1.0.69299

--- data/Standard/nondisenchantable.lua
+++ data/Standard/nondisenchantable.lua
@@ -6608,6 +6608,7 @@ addon.data.nondisenchantable = {
 	[276532] = true, -- Cursebreaker's Bracers
 	[276533] = true, -- Cursebreaker's Wristwraps
 	[276534] = true, -- Cursebreaker's Cuffs
+	[276625] = true, -- Puffin Pack
 	[276641] = true, -- Corroded Cursebreaker's Vambraces
 	[276642] = true, -- Corroded Cursebreaker's Bracers
 	[276643] = true, -- Corroded Cursebreaker's Wristwraps
