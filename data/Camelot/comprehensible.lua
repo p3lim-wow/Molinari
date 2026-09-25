cfba568679f8be0fa414a4e599344e05371593cb
p3lim <26496+p3lim@users.noreply.github.com>
2026-09-25 22:35:37 +0000

    Update forever data to 1.60.1.70009

---
+++ data/Camelot/comprehensible.lua
@@ -0,0 +1,21 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.comprehensible = {
+	[211780] = 1, -- Scroll: KWYJIBO
+	[211784] = 15, -- Scroll: WUBBA WUBBA
+	[211785] = 1, -- Scroll: CWAL
+	[211786] = 1, -- Scroll: CHAP BALK WELLES
+	[211787] = 1, -- Scroll: LOWER PING WHOMEVER
+	[211853] = 15, -- Scroll: VOCE WELL
+	[211854] = 15, -- Scroll: OMIT KESA
+	[211855] = 15, -- Scroll: STHENIC LUNATE
+	[213543] = 50, -- Scroll: UPDOG
+	[213544] = 50, -- Scroll: TOPAZ YORAK
+	[213545] = 50, -- Scroll: PEATCHY ATTAX
+	[213546] = 50, -- Scroll: SHOOBEEDOOP
+	[213547] = 50, -- Scroll: THAW WORDS
+	[281015] = 175, -- Scroll: DOST OREM
+	[281016] = 175, -- Scroll: FORGOT HOOF THUD
+	[281017] = 175, -- Scroll: KEEP CLEF FOCI
+	[281018] = 175, -- Scroll: RARE SERVICHI RADAR
+}
---
+++ data/Camelot/openable.lua
@@ -0,0 +1,30 @@
+-- this file is auto-generated
+local _, addon = ...
+addon.data.openable = {
+	[4632] = 1, -- Ornate Bronze Lockbox
+	[4633] = 25, -- Heavy Bronze Lockbox
+	[4634] = 70, -- Iron Lockbox
+	[4636] = 125, -- Strong Iron Lockbox
+	[4637] = 175, -- Steel Lockbox
+	[4638] = 225, -- Reinforced Steel Lockbox
+	[5046] = 70, -- Locked Gift
+	[5758] = 225, -- Mithril Lockbox
+	[5759] = 225, -- Thorium Lockbox
+	[5760] = 225, -- Eternium Lockbox
+	[6354] = 1, -- Small Locked Chest
+	[6355] = 70, -- Sturdy Locked Chest
+	[6712] = 1, -- Practice Lock
+	[7209] = 1, -- Tazan's Satchel
+	[7869] = 70, -- Lucius's Lockbox
+	[12033] = 275, -- Thaurissan Family Jewels
+	[13875] = 175, -- Ironbound Locked Chest
+	[13918] = 250, -- Reinforced Locked Chest
+	[16882] = 1, -- Battered Junkbox
+	[16883] = 70, -- Worn Junkbox
+	[16884] = 175, -- Sturdy Junkbox
+	[16885] = 250, -- Heavy Junkbox
+	[24282] = 5000, -- Rogue's Diary
+	[208838] = 1, -- Dark Iron Lockbox
+	[227937] = 175, -- Puzzle Box
+	[241233] = 305, -- [DNT] Super Strong Lockbox
+}
